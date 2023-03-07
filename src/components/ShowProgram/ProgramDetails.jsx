53f382b6900bcffebbe4debe5c399c6292312ce9
Mohammad Hassan <95434322+CriDeZ@users.noreply.github.com>
2023-03-07 11:04:21 +0200

    Style The ShowDetailsProgram

--- src/components/ShowProgram/ProgramDetails.jsx
+++ src/components/ShowProgram/ProgramDetails.jsx
@@ -10,19 +10,26 @@ export function ProgramDetails() {
   if (!program) {
     return <div>Loading...</div>;
   }
-
+  const OnBack = ()=>{
+    window.location.href="/show-program"
+  }
   return (
     <div className="program-details-container">
+       <div className='program-details-nav'>
+          <h2>{program.name}</h2>
+          <button onClick={OnBack} className='program-details-button'>Back</button>
+        </div>
       <div className="program-details">
-        <h2>{program.name}</h2>
-        <p>{program.description}</p>
-        <p>{program.dificulty_level}</p>
+       
+        <p style={{color:"#373a3a"}}>{program.description}</p>
+        <p style={{color:"#373a3a"}}>{program.dificulty_level}</p>
         <div className="program-days">
           {program.days.map((day, index) => (
             <div key={index} className="program-day">
               <div className='day-title'>{day.dayName}</div>
               {day.exercises.map((exercise, index) => (
                 <div key={index} className="program-exercise">
+                  <p>{"Exersice"+" "+(index+1)}</p>
                   <p>{exercise.name}</p>
                   <p>{exercise.description}</p>
                 </div>

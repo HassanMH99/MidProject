eaa72cd89d0054f51f3057fb38b492ce2a6518ac
Mohammad Hassan <95434322+CriDeZ@users.noreply.github.com>
2023-03-07 13:00:37 +0200

    The Style of TrainerHome is done

--- src/components/Trainer/TrainerHome.jsx
+++ src/components/Trainer/TrainerHome.jsx
@@ -3,34 +3,51 @@ import { db,auth } from "../../Firebase/Firebase";
 import { Link } from "react-router-dom";
 import {  onAuthStateChanged } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
-
+import './Trainer.css'
 export function TrainerHome() {
-  const [userName, setUserName] = useState("");
+  // const [userName, setUserName] = useState("");
 
-  useEffect(() => {
-    onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        try {
-          const userDocRef = doc(db, "users", user.uid);
-          const userDoc = await getDoc(userDocRef);
-          if (userDoc.exists()) {
-            setUserName(userDoc.data().name);
-          } else {
-            console.log("User document not found");
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   onAuthStateChanged(auth, async (user) => {
+  //     if (user) {
+  //       try {
+  //         const userDocRef = doc(db, "users", user.uid);
+  //         const userDoc = await getDoc(userDocRef);
+  //         if (userDoc.exists()) {
+  //           setUserName(userDoc.data().name);
+  //         } else {
+  //           console.log("User document not found");
+  //         }
+  //       } catch (error) {
+  //         console.error(error);
+  //       }
+  //     }
+  //   });
+  // }, []);
 
   return (
-    <div>
-      <h1>Welcome Trainer {userName}!</h1>
-      <Link to="/beginner-programs">Beginner Programs</Link>
-      <Link to="/intermediate-programs">Intermediate Programs</Link>
-      <Link to="/advanced-programs">Advanced Programs</Link>
+    <div className="TrainerHome">
+      <div className="TrainerHome-nav">
+        <h1>Welcome To My Website Workout </h1>
+      </div >
+      <div className="TrainerHome-h2">
+        <h2>Check your Type To Start Your Program</h2>
+      </div>
+      <div className="TrainerHome-links">
+        <div className="Link">
+          <Link to="/beginner-programs">Beginner Programs</Link>
+        </div>
+        <div className="Link">
+          <Link to="/intermediate-programs">Intermediate Programs</Link>
+          </div>
+          <div className="Link">
+        <Link to="/advanced-programs">Advanced Programs</Link>
+      </div>
+      </div>
+      <div className="TrainerHome-Description">
+        <h2>About Our Website</h2>
+        <h3>Welcome to our workout website! We provide a variety of fitness programs and exercises designed to help you achieve your health and fitness goals. Whether you're a beginner or an experienced athlete, our programs are tailored to your level of fitness and experience. Our team of experienced trainers and coaches are here to guide you every step of the way, offering tips and advice to help you maximize your results. Our website features easy-to-use navigation and a user-friendly interface, making it easy for you to find the programs and exercises that best suit your needs. So, join us today and start your journey towards a healthier, fitter you!</h3>
+      </div>
     </div>
   );
 }
